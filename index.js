0c23a4c2afd5c5afb9d49f1a5d982a7a69c9ed65
danielgoss22 <daniel.goss22@gmail.com>
2023-12-17 13:23:01 -0700

    added initial questions to array

--- index.js
+++ index.js
@@ -3,7 +3,32 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is the title of you Project?",
+    name: "title",
+    when: () => options.name,
+  },
+  {
+    type: "checkbox",
+    message: "What sections would you like to add to your README?",
+    choices: [
+      "Description",
+      "Table of Contents",
+      "Instalation",
+      "Usage",
+      "License",
+      "Contributing",
+      "Tests",
+      "Questions",
+    ],
+    name: "sections",
+    when: () => options.choices,
+  },
+];
+
+inquirer.prompt([]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}

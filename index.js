18275933b33e015ae50d888a8e81ba820230cb57
danielgoss22 <daniel.goss22@gmail.com>
2023-12-18 09:41:08 -0700

    updated index.js with all questions inside .prompt, and added begining of markdown generator as well as license functions

--- index.js
+++ index.js
@@ -1,103 +1,87 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+// console.log(generateMarkdown);
 
 // TODO: Create an array of questions for user input
-const questions = [
-  {
-    type: "input",
-    message: "What is the title of you Project?",
-    name: "title",
-  },
-  {
-    type: "input",
-    message: "What would you like in your Description?",
-    name: "description",
-  },
-  {
-    type: "input",
-    message: "What would you like in your Table of Contents?",
-    name: "tableOfContent",
-  },
-  {
-    type: "input",
-    message: "What would you like in your Installation Instructions?",
-    name: "installation",
-  },
-  {
-    type: "input",
-    message: "What would you like in your Usage Instructions?",
-    name: "usage",
-  },
-  {
-    type: "checkbox",
-    message: "What Licensing did you use?",
-    choices: [
-      "Academic Free License v3.0",
-      "Apache license 2.0",
-      "Artistic license 2.0",
-      "Boost Software License 1.0",
-      "BSD 2-clause Simplified license",
-      "BSD 3-clause New or Revised license",
-      "BSD 3-clause Clear license",
-      "BSD 4-clause Original or Old license",
-      "BSD Zero-Clause license",
-      "Creative Commons license family",
-      "Creative Commons Zero v1.0 Universal",
-      "Creative Commons Attribution 4.0",
-      "Creative Commons Attribution ShareAlike 4.0",
-      "Do What The F*ck You Want To Public License",
-      "Educational Community License v2.0",
-      "Eclipse Public License 1.0",
-      "Eclipse Public License 2.0",
-      "European Union Public License 1.1",
-      "GNU Affero General Public License v3.0",
-      "GNU General Public License family",
-      "GNU General Public License v2.0",
-      "GNU General Public License v3.0",
-      "GNU Lesser General Public License family",
-      "GNU Lesser General Public License v2.1",
-      "GNU Lesser General Public License v3.0",
-      "ISC",
-      "LaTeX Project Public License v1.3c",
-      "Microsoft Public License",
-      "MIT",
-      "Mozilla Public License 2.0",
-      "Open Software License 3.0",
-      "PostgreSQL License",
-      "SIL Open Font License 1.1",
-      "University of Illinois/NCSA Open Source License",
-      "The Unlicense",
-      "zLib License",
-    ],
-    name: "license",
-  },
-  {
-    type: "input",
-    message: "Who were the Contributors to this application?",
-    name: "contribute",
-  },
-  {
-    type: "input",
-    message: "What Tests needed to be run?",
-    name: "test",
-  },
-  {
-    type: "input",
-    message: "What is your GitHub profile?",
-    name: "github",
-  },
-  {
-    type: "input",
-    message: "What is your Email?",
-    name: "email",
-  },
-];
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is the title of you Project?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "What would you like in your Description?",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "What would you like in your Table of Contents?",
+      name: "tableOfContent",
+    },
+    {
+      type: "input",
+      message: "What would you like in your Installation Instructions?",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "What would you like in your Usage Instructions?",
+      name: "usage",
+    },
+    {
+      type: "list",
+      message: "What Licensing did you use?",
+      choices: [
+        "Apache license 2.0",
+        "Eclipse Public License 1.0",
+        "ISC",
+        "The Unlicense",
+        "MIT",
+        "none",
+      ],
+      name: "license",
+    },
+    {
+      type: "input",
+      message: "Who were the Contributors to this application?",
+      name: "contribute",
+    },
+    {
+      type: "input",
+      message: "What Tests needed to be run?",
+      name: "test",
+    },
+    {
+      type: "input",
+      message: "What is your GitHub profile?",
+      name: "github",
+    },
+    {
+      type: "input",
+      message: "What is your Email?",
+      name: "email",
+    },
+  ])
+  .then(function (userInfo) {
+    const readMeTemplate = generateMarkdown(userInfo);
+    console.log(readMeTemplate);
+    fs.writeFile("README2.md", readMeTemplate, function (err) {
+      err
+        ? console.log("An error has taken place.")
+        : console.log("Your information has been written to the file.");
+    });
+  });
 
-inquirer.prompt([]);
+// inquirer.prompt([questions]).then(function (data) {
+//   fs.writeFile("generateMarkdown.js", JSON.stringify(data));
+// });
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,68 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "MIT") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  }
+  if (license === "ISC") {
+    return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+  }
+  if (license === "Apache license 2.0") {
+    return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  }
+  if (license === "Eclipse Public License 1.0") {
+    return "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
+  }
+  if (license === "The Unlicense") {
+    return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
+  }
+  if (license === "none") {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "none") {
+    return "";
+  } else {
+    return "- [License](#license)";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "none") {
+    return "";
+  } else {
+    return "## License";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
 
+
+## Description
+${data.description}
+
+## Table of Contents 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+${renderLicenseLink(data.license)}
+  
+## Installation
+${data.instalation}
+
+## License
+
+I am using ${data.license}
 `;
 }
 
